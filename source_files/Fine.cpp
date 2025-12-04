81e9e9dfc7a5bc00205d66bdcf71a363a0b6eb59
gisneiryg <gisneiryg30@gmail.com>
2025-12-04 00:13:42 -0500

    Update Fine.cpp

--- source_files/Fine.cpp
+++ source_files/Fine.cpp
@@ -2,7 +2,7 @@
 #include <iomanip>
 #include "Fine.h" // include the Fine class header
 
-using namespace std; 
+ 
 
 // Constructor that sets the fine amount
 Fine::Fine(double amount_)
@@ -20,9 +20,9 @@ Fine Fine::operator+(const Fine& other) const {
 }
 
 // Converts the fine amount to a formatted string (example: "5.00")
-string Fine::toString() const {
-    ostringstream oss; // create a stream to build the string
-    oss << fixed << setprecision(2) << amount; // keep 2 decimal places
+std::string Fine::toString() const {
+    std::ostringstream oss; // create a stream to build the string
+    oss << std::fixed << std::setprecision(2) << amount; // keep 2 decimal places
     return oss.str(); // return string version of amount
 }
 
