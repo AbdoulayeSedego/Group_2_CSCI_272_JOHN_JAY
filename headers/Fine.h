8d8d2c8e089aed2a685bd767fd87579bf0a8cfd5
gisneiryg <gisneiryg30@gmail.com>
2025-12-03 23:32:02 -0500

    Update Fine.h

--- headers/Fine.h
+++ headers/Fine.h
@@ -1,24 +1,32 @@
 #ifndef FINE_H
 #define FINE_H
+// this class represents a monetary fine in the system
+// it basically charges a penalty of $.50 per each day (late) they take to return the book.
 
 #include <string>   // for string return type
 #include <sstream>  // for converting amount to string
 #include <iomanip>  // for formatting decimal places
-using namespace std;
+
 
 // Represents a monetary fine in the system
 class Fine {
     double amount = 0.0; // stores the fine amount
 
 public:
-    Fine() = default; // default constructor (fine = 0)
+    Fine() = default; // default constructor when there is $0.00 
+
+//Marked "explicit" to prevent implicit conversions
+//from double to Fine because if it does it would not compile.
     explicit Fine(double amount_); // create fine with a set amount
 
     double getAmount() const; // returns fine amount
     Fine operator+(const Fine& other) const; // allows adding fines together
-    string toString() const; // returns fine amount as a formatted string
+    std:: string toString() const; // returns fine amount as a formatted string
 };
 
 #endif // FINE_H
 
-// Emma Das
+// Emma and Gisneiry
+//Emma implemented most of code.
+//Gisneiry just removed name name space std, and added more comment with explanation of the code.
+
